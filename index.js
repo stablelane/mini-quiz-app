4ca42e00053ad15b340555c18428176f7d79f3cb
stablelane <aaanshad09@gmail.com>
2024-10-30 10:07:31 +0530

    refractor code

--- index.js
+++ index.js
@@ -2,90 +2,76 @@ const Quizquestions = {
     questions: [
         {
             question: "what is the capital of india",
-            options: ['delhi', 'kokalata', 'kerala']
+            options: ['delhi', 'kokalata', 'kerala'],
+            correctOption: 'delhi'
         },
         {
             question: "what is the capital of kerala",
-            options: ['trivandrum', 'kozhikode', 'kochi']
+            options: ['trivandrum', 'kozhikode', 'kochi'],
+            correctOption: 'trivandrum'
         },
         {
-            question: "what is the my name",
-            options: ['anshad', 'dahsna', 'anshaad']
+            question: "what is my name",
+            options: ['anshad', 'dahsna', 'anshaad'],
+            correctOption: 'anshad'
         },
+        {
+            question: "jilakka",
+            options: ['anshad', 'dahsna', 'anshaad'],
+            correctOption: 'anshad'
+        },
+        {
+            question: "jilakka",
+            options: ['anshad', 'dahsna', 'anshaad'],
+            correctOption: 'dahsna'
+        }
     ]
 }
 let result = 0
 let count = 0
-document.addEventListener('click', function (e) {
-    if (e.target.id == 'correct-option') {
-        document.getElementById("correct-option-a").style.backgroundColor = 'green'
-        setTimeout(showSecondquestion, 1000)
-        result++
-        console.log(result)
-        count++
 
-    } else if (e.target.id == 'wrong-option-b' || e.target.id === 'wrong-option-c') {
-        if (count == 0) {
-            setTimeout(showSecondquestion, 1000)
-            count++
-        } else if(count == 1) {
-            setTimeout(showThirdquestion, 1000)
-            count++
-        } else if(count == 2) {
-            setTimeout(showresult, 1000)
-        }
-        document.getElementById(e.target.id).previousElementSibling.style.backgroundColor = 'red'
-    } else if (e.target.id == 'correct-option-b') {
-        document.getElementById("correct-option-b").style.backgroundColor = 'green'
-        setTimeout(showThirdquestion, 1000)
-        result++
-        console.log(result)
-        count++
-
-    } else if (e.target.id == 'correct-option-c') {
-        document.getElementById("correct-option-c").style.backgroundColor = 'green'
-        setTimeout(showresult, 1000)
-        result++
-        console.log(result)
-
-    }
-})
-
-document.getElementById('container').innerHTML = `<div class="question-a" id="question-a">
-            <p>${Quizquestions.questions[0].question}</p>
-            <label id="correct-option-a" for="option-a">${Quizquestions.questions[0].options[0]}</label>
-            <input type="checkbox" name="option-a" id="correct-option">
-            <label for="option-b">${Quizquestions.questions[0].options[1]}</label>
-            <input type="checkbox" name="option-b" id="wrong-option-b">
-            <label for="option-c">${Quizquestions.questions[0].options[2]}</label>
-            <input type="checkbox" name="option-c" id="wrong-option-c">
-        </div>`
-
-function showSecondquestion() {
+function addquestion(id = 0) {
     document.getElementById('container').innerHTML = `<div class="question-a" id="question-a">
-            <p>${Quizquestions.questions[1].question}</p>
-            <label id="correct-option-b" for="option-a">${Quizquestions.questions[1].options[0]}</label>
-            <input type="checkbox" name="option-a" id="correct-option-b">
-            <label for="option-b">${Quizquestions.questions[1].options[1]}</label>
-            <input type="checkbox" name="option-b" id="wrong-option-b">
-            <label for="option-c">${Quizquestions.questions[1].options[2]}</label>
-            <input type="checkbox" name="option-c" id="wrong-option-c">
+            <p>${Quizquestions.questions[id].question}</p>
+            <label for="option-a">${Quizquestions.questions[id].options[0]}</label>
+            <input type="radio" name="options" id="option-a" class="option">
+            <label for="option-b">${Quizquestions.questions[id].options[1]}</label>
+            <input type="radio" name="options" id="option-b" class="option">
+            <label for="option-c">${Quizquestions.questions[id].options[2]}</label>
+            <input type="radio" name="options" id="option-c" class="option">
         </div>`
+    document.querySelectorAll('.option').forEach(item => item.addEventListener('click',(e) => checkAnswer(e,count)))
 }
 
-function showThirdquestion() {
-    document.getElementById('container').innerHTML = `<div class="question-a" id="question-a">
-            <p>${Quizquestions.questions[2].question}</p>
-            <label id="correct-option-c" for="option-a">${Quizquestions.questions[2].options[0]}</label>
-            <input type="checkbox" name="option-a" id="correct-option-c">
-            <label for="option-b">${Quizquestions.questions[2].options[1]}</label>
-            <input type="checkbox" name="option-b" id="wrong-option-b">
-            <label for="option-c">${Quizquestions.questions[2].options[2]}</label>
-            <input type="checkbox" name="option-c" id="wrong-option-c">
-        </div>`
-}      
+addquestion()
 
+function checkAnswer(e, questionNo) {
+    if (e.target.labels[0].innerText == Quizquestions.questions[questionNo].correctOption) {
+        console.log("correct item")
+        e.target.labels[0].style.backgroundColor = 'green'
+        count++
+        result++
+        if (count < Quizquestions.questions.length) {
+            setTimeout(addquestion, 1000, count)
+
+        } else {
+            setTimeout(showresult, 1000)
+        }
+
+    } else {
+        console.log("wrong item")
+        e.target.labels[0].style.backgroundColor = 'red'
+        count++
+        if (count < Quizquestions.questions.length) {
+            setTimeout(addquestion, 1000, count)
+
+        } else {
+            setTimeout(showresult, 1000)
+        }
+    }
+    console.log(count,result)
+}
 
 function showresult() {
     document.getElementById('container').innerHTML = `<h1>your result is ${result}</h1>`
-} 
+}
